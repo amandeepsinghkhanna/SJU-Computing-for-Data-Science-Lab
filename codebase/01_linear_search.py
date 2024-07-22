bad9aac0f46355f439afcb8d498a26c35b40ce8e
amandeepsinghkhanna <amandeepsinghkhanna@gmail.com>
2024-07-22 07:44:39 +0530

    Added the code for the linear search algorithm

---
+++ codebase/01_linear_search.py
@@ -0,0 +1,27 @@
+"""
+    Algorithm Number: 01
+    Algorithm Name: 'Linear Search'
+    Script Author: Amandeep Singh Khanna
+"""
+
+# Defining the function for linear search:
+def linear_search(search_element: int,search_lst:list)->str:
+    for lst_idx in range(len(search_element)):
+        if search_element == search_element[lst_idx]:
+            return f"Element {search_element} found at list index {lst_idx}"
+        else:
+            continue
+    return "Element not in the list"
+
+# Defining the list to be searched
+search_lst = [10, 20, 2, 1, 33, 4]
+search_element = 2 # Element to be searched for
+
+# Searching for element 2 in the above specified list
+print(
+    linear_search(
+        search_element=search_element, 
+        search_lst=search_lst
+    )
+)
+
