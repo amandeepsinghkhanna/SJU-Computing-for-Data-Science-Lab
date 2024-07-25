726ed15939f1c5b13defb414d7752975b7e847c2
amandeepsinghkhanna <amandeepsinghkhanna@gmail.com>
2024-07-25 22:39:23 +0530

    updates to the linkedlist

--- codebase/01_datastructures/03_linked_list.py
+++ codebase/01_datastructures/03_linked_list.py
@@ -5,6 +5,7 @@
 """
 
 
+# %%
 class Node:
     """
     Defining a custom object for capturing data and the next node.
@@ -22,19 +23,35 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+        self.size = 0
 
-    def insert_element_at_end(self, data):
+    def insert_element_at_start(self, data) -> None:
+        """
+        Adds an element at the begining of the linked list.
+        """
+        if self.head == None:
+            self.head = Node(data)
+            self.size += 1
+        else:
+            first_node = self.head.next_node
+            self.head = Node(data)
+            self.head.next_node = first_node
+            self.size += 1
+
+    def insert_element_at_end(self, data) -> None:
         """ """
         if self.head == None:  # If the linked list is empty.
             self.head = Node(data)
+            self.size += 1
         else:  # If the linked list has at least one value.
             current_node = self.head
             while current_node.next_node != None:
                 current_node = current_node.next_node
             current_node.next_node = Node(data)
+            self.size += 1
 
-    def insert_element_at_position(self):
-        """"""
+    def insert_element_at_position(self, data: object, position: int) -> None:
+        """ """
         pass
 
     def print_linkedlist(self):
@@ -55,15 +72,29 @@ def main():
     The main program.
     """
     linked_list = LinkedList()  # Initializing the linked list.
+    print(f"The size of the linked list {linked_list.size}")
     linked_list.insert_element_at_end(10)  # Inserting element at the end.
+    print(f"The size of the linked list {linked_list.size}")
     linked_list.print_linkedlist()  # Printing the linked list.
+    print(f"The size of the linked list {linked_list.size}")
     linked_list.insert_element_at_end(1)  # Inserting element at the end.
+    print(f"The size of the linked list {linked_list.size}")
     linked_list.print_linkedlist()  # Printing the linked list.
+    print(f"The size of the linked list {linked_list.size}")
     linked_list.insert_element_at_end(11)  # Inserting element at the end.
+    print(f"The size of the linked list {linked_list.size}")
     linked_list.print_linkedlist()  # Printing the linked list.
+    print(f"The size of the linked list {linked_list.size}")
     linked_list.insert_element_at_end(13)  # Inserting element at the end.
+    print(f"The size of the linked list {linked_list.size}")
+    linked_list.print_linkedlist()  # Printing the linked list.
+    print(f"The size of the linked list {linked_list.size}")
+    linked_list.insert_element_at_start(0)  # Inserting element at the start.
+    print(f"The size of the linked list {linked_list.size}")
     linked_list.print_linkedlist()  # Printing the linked list.
 
 
 if __name__ == "__main__":
     main()
+
+# %%
